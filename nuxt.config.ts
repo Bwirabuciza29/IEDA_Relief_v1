a16900d821b3164e8f5e7d41aa8f76874d97aef9
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-15 11:19:03 +0200

    LetsGo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,32 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    langDir: "locales",
+    strategy: "prefix_except_default",
+    locales: [
+      { code: "fr-FR", iso: "fr-FR", name: "français", file: "fr-FR.json" },
+      { code: "en-UK", iso: "en-UK", name: "English (UK)", file: "en-UK.json" },
+      {
+        code: "sw-CD",
+        iso: "sw-CD",
+        name: "Swahili (Congo)",
+        file: "sw-CD.json",
+      },
+      { code: "es-ES", iso: "es-ES", name: "Español", file: "es-ES.json" },
+      { code: "in-HI", iso: "in-HI", name: "हिन्दी", file: "in-HI.json" },
+    ],
+    defaultLocale: "en-UK",
+    vueI18n: "./i18n.config.js",
   },
 });
