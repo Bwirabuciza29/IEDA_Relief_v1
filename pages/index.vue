cc02baca9a13ce30cd0d5957362a45660a6f4c9b
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-15 14:08:33 +0200

    WeJustSmile

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="rm-wrapper">
+  <div>
     <div class="mj-container mt-24">
       Blondy Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
       excepturi et repellendus dolores voluptate cum dicta ratione quidem veniam
-      fugit
+      fugit Blondy Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Explicabo excepturi et repellendus dolores voluptate cum dicta ratione
+      quidem veniam fugit Blondy Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Explicabo excepturi et repellendus dolores voluptate cum
+      dicta ratione quidem veniam fugit Blondy Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Explicabo excepturi et repellendus dolores
+      voluptate cum dicta ratione quidem veniam fugit
     </div>
-    <div>
+    <div class="mj-container">
       <home />
     </div>
   </div>
