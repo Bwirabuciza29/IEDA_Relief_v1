f3a19c7086ef5054413b3e521319c03c71a4929e
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-18 12:25:01 +0200

    THatsGreat

--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,29 @@
   <div>
     <div class="relative mt-20">
       <!-- Contenu principal -->
-      <div class="mj-container p-6 relative z-10">
+      <div class="mj-container relative z-10">
         <div
-          class="flex flex-col md:flex-row items-center md:justify-between px-6 md:px-20 py-10"
+          class="flex flex-col md:flex-row items-center md:justify-between px-6 py-10"
         >
           <!-- Section de gauche -->
           <div class="md:w-1/2 space-y-6 text-center md:text-left">
             <h1 class="text-4xl md:text-5xl">
-              Helping <span class="text-green-500 font-sri">Communities</span>
-
-              to become
-              <span class="text-green-500 font-sri">SelfSufficient</span
+              {{ title_1 }}
+              <span class="text-green-500 font-sri"> {{ title_2 }} </span>
+              {{ title_3 }}
+              <span class="text-green-500 font-sri"> {{ title_4 }} </span
               >....<img
                 src="/img/Assets_1.png"
                 class="w-8 h-8 md:w-12 md:h-12 -mt-4 inline-block ml-2"
               />
             </h1>
             <p class="text-gray-700 text-lg">
-              Helping others and making a difference in the world is what will
-              help to make the world a better place by improving people's lives.
-              One person at a time, one day at a time, and one project at a
-              time, you can make a difference that will leave a lasting impact
-              on the world.
+              {{ desc }}
             </p>
             <button
               class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md"
             >
-              DONATE NOW
+              {{ btn }}
             </button>
           </div>
 
@@ -41,35 +37,33 @@
               <img
                 src="/img/img_principal.png"
                 alt="Community support"
-                class="shadow-lg transform rounded-lg transition hover:scale-105"
+                class="shadow-lg transform rounded-lg"
               />
               <!-- Image en haut à gauche -->
               <img
                 src="/img/img_1.png"
                 alt="Volunteer"
-                class="absolute top-[50%] left-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md transform rounded-lg transition hover:scale-105"
+                class="absolute top-[50%] left-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md rounded-lg transition-all duration-300 hover:z-10 hover:scale-110"
               />
 
               <!-- Image en haut à droite -->
               <img
                 src="/img/img_2.png"
                 alt="Child"
-                class="absolute top-[20%] right-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md transform rounded-lg transition hover:scale-105"
+                class="absolute top-[20%] right-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md transform rounded-lg transition-all duration-300 hover:z-10 hover:scale-110"
               />
             </div>
 
             <!-- Carte en bas (superposée) -->
             <div
-              class="bg-green-400 p-2 md:p-3 rounded-md flex items-center justify-center w-3/4 md:w-1/2 absolute -bottom-8 left-[13%] transform -translate-x-[50%] md:translate-x-[50%] transition hover:scale-105"
+              class="bg-green-400 p-2 rounded-md flex items-center justify-center w-2/4 md:w-2/2 absolute -bottom-16 left-44 transform -translate-x-1/2 md:translate-x-0 transition hover:scale-105 mobile:bottom-8 mobile:left-1/2 mobile:transform mobile:-translate-x-1/2"
             >
               <div class="text-center">
-                <p
-                  class="text-2xl md:text-3xl font-semibold text-black font-sri"
-                >
-                  2.2 million
+                <p class="text-2xl md:text-3xl text-black font-sri">
+                  {{ card_num }}
                 </p>
-                <p class="text-gray-700 text-sm md:text-base">
-                  People in Our Programs Worldwide
+                <p class="text-black hover:underline">
+                  {{ card_desc }}
                 </p>
               </div>
             </div>
@@ -90,3 +84,14 @@
     </div>
   </div>
 </template>
+<script setup>
+const { t } = useI18n();
+const title_1 = ref(t("hero.title_1"));
+const title_2 = ref(t("hero.title_2"));
+const title_3 = ref(t("hero.title_3"));
+const title_4 = ref(t("hero.title_4"));
+const desc = ref(t("hero.desc"));
+const btn = ref(t("hero.btn"));
+const card_num = ref(t("hero.card_num"));
+const card_desc = ref(t("hero.card_desc"));
+</script>
