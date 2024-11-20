a73ff68039587d992b87aa7281e140f76fe4b319
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-20 21:14:44 +0200

    LetsWorkOnIt

--- pages/works/[id].vue
+++ pages/works/[id].vue
@@ -1,36 +1,296 @@
 <template>
-  <div class="container mx-auto my-24 p-4">
-    <NuxtLink to="/works" class="mb-8 bg-gray-200 px-4 py-2 rounded-lg">
-      Back
-    </NuxtLink>
+  <section>
+    <div v-if="card">
+      <div class="relative bg-custom-green p-8 mx-4 rounded-lg mt-20">
+        <div class="mj-container">
+          <div class="relative">
+            <div class="rounded-lg flex flex-col md:flex-row overflow-hidden">
+              <div class="flex-1 p-6 md:p-8">
+                <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold mb-2">
+                  {{ card.title }}
+                </h1>
 
-    <!-- Vérification que card existe -->
-    <div v-if="card" class="bg-white shadow-md rounded-lg overflow-hidden">
-      <img
-        :src="card.image"
-        alt="Card Image"
-        class="w-full h-60 object-cover"
-      />
-      <div class="p-6">
-        <h1 class="text-2xl font-bold mb-4">{{ card.title }}</h1>
-        <p class="text-gray-600 mb-4">{{ card.description }}</p>
-        <p class="text-gray-500">
-          Additional details about {{ card.title }}...
-        </p>
+                <nav class="text-sm text-black">
+                  <div class="inline-block bg-white p-3 rounded-lg">
+                    <a href="/works" class="hover:underline font-semibold"
+                      >Works</a
+                    >
+                    /
+                    <span>{{ card.title }}</span>
+                  </div>
+                </nav>
+              </div>
+              <div class="flex-1 hidden md:flex">
+                <img
+                  :src="card.image"
+                  alt="About Us Section"
+                  class="w-full h-48 object-cover rounded-lg transition-all duration-300 hover:z-10 hover:scale-105"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mj-container my-8">
+        <div
+          class="flex flex-col md:flex-row p-6 rounded-lg space-y-6 md:space-y-0 md:space-x-8"
+        >
+          <!-- Section Gauche -->
+          <div class="flex-1">
+            <h1 class="text-4xl font-bold text-black mb-4">
+              {{ card.title }}, which means "land of honest men"
+            </h1>
+            <p class="text-gray-600">
+              Since the Mali crisis in 2012, WFP signed a tripartite accord
+              (UNHCR, WFP, and IEDA Relief) to ensure food distribution to
+              33,000 Mali refugees living in and outside the camps.
+            </p>
+            <p class="text-gray-600 mt-4">
+              Food distribution is done on a monthly basis and it includes rice,
+              CSB, oil, salt, and cereals. WFP avails about 6888 tonnes (574
+              tonnes monthly) through UNHCR, which pays for the operational
+              costs at around 180,000 USD.
+            </p>
+          </div>
+
+          <!-- Section Droite -->
+          <div class="w-full md:w-1/3 bg-white rounded-lg shadow-md p-4">
+            <ul class="space-y-4">
+              <!-- Item 1 -->
+              <li class="flex items-center space-x-3">
+                <span
+                  class="flex items-center justify-center w-8 h-8 bg-custom-green text-white rounded-full"
+                >
+                  <i class="fa-solid fa-calendar-days"></i>
+                </span>
+                <p class="text-gray-800 text-sm font-medium">November, 1</p>
+              </li>
+
+              <!-- Item 2 -->
+              <li class="flex items-center space-x-3">
+                <span
+                  class="flex items-center justify-center w-8 h-8 bg-custom-green text-white rounded-full"
+                >
+                  <i class="fa-solid fa-city"></i>
+                </span>
+                <p class="text-gray-800 text-sm font-medium">Ouagadougou</p>
+              </li>
+
+              <!-- Item 3 -->
+              <li class="flex items-center space-x-3">
+                <span
+                  class="flex items-center justify-center w-8 h-8 bg-custom-green text-white rounded-full"
+                >
+                  <i class="fa-solid fa-tents"></i>
+                </span>
+                <p class="text-gray-800 text-sm font-medium">Welcome City</p>
+              </li>
+
+              <!-- Item 4 -->
+              <li class="flex items-center space-x-3">
+                <span
+                  class="flex items-center justify-center w-8 h-8 bg-custom-green text-white rounded-full"
+                >
+                  <i class="fa-solid fa-car"></i>
+                </span>
+                <p class="text-gray-800 text-sm font-medium">10km or 2.5kr</p>
+              </li>
+            </ul>
+
+            <p class="text-gray-800 font-bold border-t border-b py-2 mt-4">
+              Potential: $500 - $5,000
+            </p>
+            <button
+              class="mt-6 w-full bg-custom-green text-white py-2 px-4 rounded-lg hover:bg-green-600 transition"
+            >
+              REGISTER NOW
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="mj-container my-8">
+        <div
+          class="flex flex-col md:flex-row p-6 rounded-lg space-y-6 md:space-y-0 md:space-x-8"
+        >
+          <!-- Section Gauche -->
+          <div class="flex-1">
+            <div class="relative mb-6">
+              <img
+                :src="card.image"
+                alt="IEDA Relief Activities"
+                class="w-full h-64 object-cover rounded-lg"
+              />
+              <div
+                class="absolute inset-0 flex items-center justify-center"
+                aria-label="Play Video"
+              >
+                <img src="/img/Play.png" alt="Play" class="w-20 h-20" />
+              </div>
+            </div>
+
+            <!-- Description principale -->
+            <div>
+              <h2 class="text-xl font-bold text-gray-800 mb-4">
+                IEDA Relief organizes the following activities:
+              </h2>
+              <p class="text-gray-600 mb-4">
+                Mattis vulputate enim nulla aliquet porttitor lacus luctus
+                accumsan tortor. Sed nisi lacus sed viverra tellus.
+              </p>
+
+              <!-- Activités listées -->
+              <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div>
+                  <h3 class="text-lg font-semibold text-gray-800">
+                    Monthly distribution plans (MDP)
+                  </h3>
+                  <p class="text-gray-600">
+                    Facilisis volutpat est velit egestas dui id ornare arcu
+                    pretium aenean pharetra odio.
+                  </p>
+                </div>
+                <div>
+                  <h3 class="text-lg font-semibold text-gray-800">
+                    Evaluate the Refugee
+                  </h3>
+                  <p class="text-gray-600">
+                    Evaluate the refugee feeding situation, specifically those
+                    in specific needs.
+                  </p>
+                </div>
+                <div>
+                  <h3 class="text-lg font-semibold text-gray-800">
+                    Avail Blanket supplementary
+                  </h3>
+                  <p class="text-gray-600">
+                    Avail Blanket supplementary Feeding to young girls and boys.
+                  </p>
+                </div>
+                <div>
+                  <h3 class="text-lg font-semibold text-gray-800">
+                    Organizing joint meeting prior
+                  </h3>
+                  <p class="text-gray-600">
+                    Organizing joint meeting prior and after distribution to
+                    ensure accountability.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Section Volunteer -->
+            <div class="mt-6">
+              <h2 class="text-xl font-bold text-gray-800 mb-2">
+                Volunteer with Us
+              </h2>
+              <p class="text-gray-600 mb-4">
+                Egestas pretium aenean pharetra magna ac placerat. Nisi lacus
+                sed viverra tellus.
+              </p>
+              <NuxtLink
+                to="/works"
+                class="text-custom-blue underline hover:text-blue-600 font-semibold"
+                >BACK TO WORK!
+              </NuxtLink>
+            </div>
+            <hr class="my-6" />
+            <!-- Tags sociaux -->
+
+            <!-- Social Icons -->
+            <div
+              class="flex flex-col sm:flex-row justify-between items-center flex-wrap gap-4 mt-6"
+            >
+              <!-- Tags -->
+              <div
+                class="flex flex-wrap gap-2 justify-center w-full sm:justify-start"
+              >
+                <span
+                  class="bg-gray-200 text-gray-700 px-4 py-2 rounded-full text-sm text-center"
+                >
+                  #SocialImpact
+                </span>
+                <span
+                  class="bg-gray-200 text-gray-700 px-4 py-2 rounded-full text-sm text-center"
+                >
+                  #ChangingLives
+                </span>
+              </div>
+              <!-- Social Icons -->
+              <div
+                class="flex items-center gap-3 justify-center w-full sm:w-auto sm:justify-end"
+              >
+                <a
+                  href="https://facebook.com"
+                  class="bg-gray-200 flex items-center justify-center p-2 rounded-full text-black hover:bg-blue-800 hover:text-white transition"
+                >
+                  <i class="fab fa-facebook-f text-lg"></i>
+                </a>
+                <a
+                  href="https://instagram.com"
+                  class="bg-gray-200 flex items-center justify-center p-2 rounded-full text-black hover:bg-pink-800 hover:text-white transition"
+                >
+                  <i class="fab fa-instagram text-lg"></i>
+                </a>
+                <a
+                  href="https://twitter.com"
+                  class="bg-gray-200 flex items-center justify-center p-2 rounded-full text-black hover:bg-gray-600 hover:text-white transition"
+                >
+                  <i class="fa-brands fa-x-twitter text-lg"></i>
+                </a>
+                <a
+                  href="https://linkedin.com"
+                  class="bg-gray-200 flex items-center justify-center p-2 rounded-full text-black hover:bg-blue-900 hover:text-white transition"
+                >
+                  <i class="fab fa-linkedin-in text-lg"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Section Droite -->
+          <div class="w-full h-48 md:w-1/3 bg-white rounded-lg shadow-md p-4">
+            <h3 class="text-lg font-bold text-gray-800 mb-4 text-center">
+              Have Additional Questions?
+            </h3>
+            <ul class="space-y-3">
+              <li class="flex items-center">
+                <i
+                  class="fas fa-envelope text-white text-xs mr-3 rounded-full p-2 bg-custom-green"
+                ></i>
+                <span class="text-gray-600">achillebondy@gmail.com</span>
+              </li>
+              <li class="flex items-center">
+                <i
+                  class="fas fa-phone-alt text-white text-xs mr-3 rounded-full p-2 bg-custom-green"
+                ></i>
+                <span class="text-gray-600">+243 999 249</span>
+              </li>
+              <li class="flex items-center">
+                <i
+                  class="fas fa-map-marker-alt text-white text-xs mr-3 rounded-full p-2 bg-custom-green"
+                ></i>
+                <span class="text-gray-600">123 Main Street, Burkina Faso</span>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
 
-    <!-- Afficher un message de chargement si card est null -->
     <div v-else class="text-center">
       <p class="text-gray-600">Loading...</p>
     </div>
-  </div>
+    <div class="my-16">
+      <LogOurs />
+    </div>
+    <!-- Footer -->
+    <div class="mt-16">
+      <laster />
+    </div>
+  </section>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
-
 const route = useRoute();
 const card = ref(null);
 
---
+++ pages/works/exemple.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="container mx-auto my-24 p-4">
+    <NuxtLink to="/works" class="mb-8 bg-gray-200 px-4 py-2 rounded-lg">
+      Back
+    </NuxtLink>
+
+    <!-- Vérification que card existe -->
+    <div v-if="card" class="bg-white shadow-md rounded-lg overflow-hidden">
+      <img
+        :src="card.image"
+        alt="Card Image"
+        class="w-full h-60 object-cover"
+      />
+      <div class="p-6">
+        <h1 class="text-2xl font-bold mb-4">{{ card.title }}</h1>
+        <p class="text-gray-600 mb-4">{{ card.description }}</p>
+        <p class="text-gray-500">
+          Additional details about {{ card.title }}...
+        </p>
+      </div>
+    </div>
+
+    <!-- Afficher un message de chargement si card est null -->
+    <div v-else class="text-center">
+      <p class="text-gray-600">Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const card = ref(null);
+
+const cards = [
+  {
+    id: 1,
+    image: "/img/w1.png",
+    tag: "Where we work",
+    title: "Burkina Faso",
+    description:
+      "Empowering communities with essential knowledge and practices to promote health and prevent",
+  },
+  {
+    id: 2,
+    image: "/img/w2.png",
+    tag: "Where we work",
+    title: "Cameroun",
+    description:
+      "Providing Harper with the freedom and independence to navigate the world with ease",
+  },
+  {
+    id: 3,
+    image: "/img/w3.png",
+    tag: "Where we work",
+    title: "Central Africa Republic",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+  {
+    id: 4,
+    image: "/img/w4.png",
+    tag: "Where we work",
+    title: "Mali",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+  {
+    id: 5,
+    image: "/img/w5.png",
+    tag: "Where we work",
+    title: "Niger",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+  {
+    id: 6,
+    image: "/img/w6.png",
+    tag: "Where we work",
+    title: "Philippines",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+  {
+    id: 7,
+    image: "/img/w7.png",
+    tag: "Where we work",
+    title: "Haiti",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+  {
+    id: 8,
+    image: "/img/w8.png",
+    tag: "Where we work",
+    title: "Democratic Rep. of the Congo",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+  {
+    id: 9,
+    image: "/img/w9.png",
+    tag: "Where we work",
+    title: "Get Involved",
+    description:
+      "Join us in providing compassionate support and resources for Daniel as he embarks on his",
+  },
+];
+
+onMounted(() => {
+  const id = parseInt(route.params.id);
+  card.value = cards.find((c) => c.id === id);
+
+  if (!card.value) {
+    console.error("Card not found");
+  }
+});
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+}
+</style>
