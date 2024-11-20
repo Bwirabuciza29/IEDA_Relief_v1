6cff21798442ca455e11b6f00c82a7de58e02851
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-20 11:15:47 +0200

    WeGoAgain

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,31 +1,102 @@
 <template>
-  <section class="relative bg-custom-green p-8 mx-4 rounded-lg my-20">
-    <!-- Image vectorielle (positionnée au-dessus du conteneur) -->
-    <img
-      src="/img/Vectoria.png"
-      alt="Decorative Vector"
-      class="absolute top-0 left-64 h-full w-auto z-10 hidden sm:block"
-    />
-
-    <div class="mj-container relative">
-      <div class="rounded-lg flex flex-col md:flex-row overflow-hidden">
-        <!-- Texte (à gauche sur grand écran) -->
-        <div class="flex-1 p-6 md:p-8">
-          <h1 class="text-4xl font-bold mb-2">About Us</h1>
-          <nav class="text-sm text-black">
-            <div class="inline-block bg-white p-3 rounded-lg">
-              <a href="/" class="hover:underline font-semibold">Home</a> /
-              <span>About Us</span>
+  <section>
+    <div class="relative bg-custom-green p-8 mx-4 rounded-lg mt-20">
+      <!-- Image vectorielle (positionnée au-dessus du conteneur) -->
+      <img
+        src="/img/Vectoria.png"
+        alt="Decorative Vector"
+        class="absolute top-0 left-64 h-full w-auto z-10 hidden sm:block"
+      />
+      <div class="mj-container">
+        <div class="relative">
+          <div class="rounded-lg flex flex-col md:flex-row overflow-hidden">
+            <!-- Texte (à gauche sur grand écran) -->
+            <div class="flex-1 p-6 md:p-8">
+              <h1 class="text-4xl font-bold mb-2">About Us</h1>
+              <nav class="text-sm text-black">
+                <div class="inline-block bg-white p-3 rounded-lg">
+                  <a href="/" class="hover:underline font-semibold">Home</a> /
+                  <span>About Us</span>
+                </div>
+              </nav>
             </div>
-          </nav>
+
+            <!-- Image (à droite sur grand écran) -->
+            <div class="flex-1">
+              <img
+                src="/img/cha.png"
+                alt="About Us Section"
+                class="w-full h-full object-cover rounded-lg transition-all duration-300 hover:z-10 hover:scale-105"
+              />
+            </div>
+          </div>
+        </div>
+        <!-- contenu de l'about -->
+      </div>
+    </div>
+    <div class="mj-container my-16">
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <!-- Card 1 -->
+        <div class="flex flex-col items-start p-4 border rounded-lg shadow-md">
+          <div class="mb-4">
+            <img src="/img/icon_1.png" alt="Icon" class="w-10 h-10" />
+          </div>
+          <h3 class="text-xl font-semibold mb-2">Vision</h3>
+          <p class="text-gray-600">
+            Our vision is to establish a stable environment with sustainable
+            solutions that benefit the population in need. We empower
+            communities...
+          </p>
         </div>
 
-        <!-- Image (à droite sur grand écran) -->
-        <div class="flex-1">
+        <!-- Card 2 -->
+        <div class="overflow-hidden border rounded-lg shadow-md">
           <img
-            src="/img/cha.png"
-            alt="About Us Section"
-            class="w-full h-full object-cover rounded-lg transition-all duration-300 hover:z-10 hover:scale-105"
+            src="/img/c1.png"
+            alt="Image 1"
+            class="w-full h-full object-cover"
+          />
+        </div>
+
+        <!-- Card 3 -->
+        <div class="flex flex-col items-start p-4 border rounded-lg shadow-md">
+          <div class="mb-4">
+            <img src="/img/icon_2.png" alt="Icon" class="w-10 h-10" />
+          </div>
+          <h3 class="text-xl font-semibold mb-2">Mission</h3>
+          <p class="text-gray-600">
+            The mission of IEDA Relief is to alleviate the suffering of
+            vulnerable people by tackling the underlying causes of poverty...
+          </p>
+        </div>
+
+        <!-- Card 4 -->
+        <div class="overflow-hidden border rounded-lg shadow-md">
+          <img
+            src="/img/c2.png"
+            alt="Image 2"
+            class="w-full h-full object-cover"
+          />
+        </div>
+
+        <!-- Card 5 -->
+        <div class="flex flex-col items-start p-4 border rounded-lg shadow-md">
+          <div class="mb-4">
+            <img src="/img/icon_3.png" alt="Icon" class="w-10 h-10" />
+          </div>
+          <h3 class="text-xl font-semibold mb-2">Value</h3>
+          <p class="text-gray-600">
+            We are committed to working in the most difficult conditions to
+            reach the world's poorest and most vulnerable people...
+          </p>
+        </div>
+
+        <!-- Card 6 -->
+        <div class="overflow-hidden border rounded-lg shadow-md">
+          <img
+            src="/img/c3.png"
+            alt="Image 3"
+            class="w-full h-full object-cover"
           />
         </div>
       </div>
@@ -35,5 +106,5 @@
 
 <script setup>
 const { t } = useI18n();
-const title_1 = ref(t("help.title_1"));
+const title_1 = ref(t("about.title_1"));
 </script>
